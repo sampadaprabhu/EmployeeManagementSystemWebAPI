2b8ff41beb5ca4b59def505df60dd355bbfe3204
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-23 22:05:06 +0530

    [SAMPADA] ADD : Employee Details Model Class To Insert The Data

---
+++ EmpManagementML/EmployeeDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EmpManagementML
+{
+    public class EmployeeDetails
+    {
+        [Required(ErrorMessage = "First Name Is Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name Is Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email ID Is Required")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Please Enter Valid Email ID")]
+        public string EmailID { get; set; }
+
+        [Required(ErrorMessage = "Phone Number Is Required")]
+        [RegularExpression(@"^([6-9]{1}[0-9]{9})$", ErrorMessage = "Please Enter Valid Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Department ID Is Required")]
+        public int DepartmentID { get; set; }
+    }
+}
