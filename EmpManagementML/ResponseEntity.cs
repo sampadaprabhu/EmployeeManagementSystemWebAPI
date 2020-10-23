3171a2e8dbf866541f5cf629b9472b6e13dd4c6d
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-23 22:06:26 +0530

    [SAMPADA] ADD : Response Entity Class For Status Code

---
+++ EmpManagementML/ResponseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace EmpManagementML
+{
+    public class ResponseEntity
+    {
+        public HttpStatusCode code;
+        public string message;
+        public object result;
+
+        public ResponseEntity(HttpStatusCode code, string message, object result)
+        {
+            this.code = code;
+            this.message = message;
+            this.result = result;
+        }
+    }
+}
