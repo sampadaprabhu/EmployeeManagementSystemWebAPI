bcc0d5d1fe7f327068d1b0bce1e75d335d0dc150
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-23 20:36:31 +0530

    [SAMPADA] ADD : Functionality To Get All Employees In Repository Layer

--- EmpManagementRL/EmpManagementRepositoryLayer.cs
+++ EmpManagementRL/EmpManagementRepositoryLayer.cs
@@ -2,6 +2,7 @@
 using EmpManagementRL.implementation;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -107,6 +108,45 @@ namespace EmpManagementRL
             catch (Exception e)
             {
                 throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        public List<EmpManagementModelLayer> GetAllEmployees()
+        {
+            try
+            {
+                List<EmpManagementModelLayer> getAllData = new List<EmpManagementModelLayer>();
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SpGetAllEmployees", this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    this.connection.Open();
+                    SqlDataReader sqlDataReader = command.ExecuteReader();
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            EmpManagementModelLayer empManagementModelLayer = new EmpManagementModelLayer();
+                            empManagementModelLayer.EmpID = Convert.ToInt32(sqlDataReader["EmpID"]);
+                            empManagementModelLayer.FirstName = sqlDataReader["FirstName"].ToString();
+                            empManagementModelLayer.LastName = sqlDataReader["LastName"].ToString();
+                            empManagementModelLayer.EmailID = sqlDataReader["EmailID"].ToString();
+                            empManagementModelLayer.PhoneNumber = sqlDataReader["PhoneNumber"].ToString();
+                            empManagementModelLayer.DepartmentID = Convert.ToInt32(sqlDataReader["DepartmentID"]);
+                            getAllData.Add(empManagementModelLayer);
+                        }
+                        return getAllData;
+                    }
+                    return null;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
             finally
             {
--- EmpManagementRL/interface/IEmpManagementRepositoryLayer.cs
+++ EmpManagementRL/interface/IEmpManagementRepositoryLayer.cs
@@ -10,5 +10,6 @@ namespace EmpManagementRL.implementation
         bool AddEmployee(EmpManagementModelLayer empManagementModelLayer);
         bool UpdateEmployee(EmpManagementModelLayer empManagementModelLayer);
         bool DeleteEmployee(int EmpID);
+        List<EmpManagementModelLayer> GetAllEmployees();
     }
 }
