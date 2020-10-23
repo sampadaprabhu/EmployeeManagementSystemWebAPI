5d63b71e2d9aa20bf5731c5daa89c38f88dd9d9f
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-23 19:24:22 +0530

    [SAMPADA] ADD : Interface In Repository Layer

--- EmpManagementRL/EmpManagementRepositoryLayer.cs
+++ EmpManagementRL/EmpManagementRepositoryLayer.cs
@@ -2,7 +2,7 @@
 
 namespace EmpManagementRL
 {
-    public class Class1
+    public class EmpManagementRepositoryLayer
     {
     }
 }
---
+++ EmpManagementRL/interface/IEmpManagementRepositoryLayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpManagementRL.implementation
+{
+    public interface IEmpManagementRepositoryLayer
+    {
+    }
+}
